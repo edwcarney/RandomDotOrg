335387dc3f14f8f3fe5b82f60eff6c2b99d28fb1
Edward Carney <carne006@gmail.com>
2019-02-11 14:42:18 -0800

    Update RandomDotOrg.jl
    Tighten up documentation and add examples.

--- RandomDotOrg.jl
+++ RandomDotOrg.jl
@@ -1,9 +1,8 @@
 module RandomDotOrg
 
 using HTTP, Printf
-
 export  getQuota, checkQuota, randomNumbers, randomSequence, randomStrings, randomGaussian,
-        randomDecimalFractions, randomBytes
+        randomDecimalFractions, randomBytes, randomBitmap
 
 """
     Get the current bit quota from Random.org
@@ -14,6 +13,9 @@ function getQuota()
 end;
 
 """
+
+    checkQuota(minimum = 500::Number)
+
     Test for sufficient quota to insure response. This should be set to match
     user's needs.
 """
@@ -22,16 +24,56 @@ function checkQuota(minimum = 500::Number)
 end;
 
 """
-    Get `n` random integers on the interval `[min, max]`
-    in one of 4 `base` values--[binary (2), octal (8), decimal (10), or hexadecimal (16)]
-    All numbers are returned as strings (as Random.org sends them).
 
-    # Arguments
-    `base::Integer`: retrieved Integer format [2, 8, 10, 16]
-    `check::Bool`: perform a call to `checkQuota` before making request
-    `col::Integer`: used to fulfill parameter requirments of random.org
+    randomNumbers(n = 100::Number, min = 1, max = 20, base = 10, parse=true, check = true, col = 5) 
+ 
+Get `n` random integers on the interval `[min, max]` as strings
+in one of 4 `base` values--[binary (2), octal (8), decimal (10), or hexadecimal (16)]
+All numbers are returned as strings (as Random.org sends them).
+
+# Arguments
+- `max`,`min` : [-1e9, 1e9]
+- `base::Integer`: retrieved Integer format [2, 8, 10, 16]
+- `numeric`: return numbers instead of strings (base = 10, only)
+- `check::Bool`: perform a call to `checkQuota` before making request
+- `col::Integer`: used to fulfill parameter requirments of random.org
+
+# Examples
+```
+julia> randomNumbers(5, max=50, base=16)
+5-element Array{SubString{String},1}:
+ "1f"
+ "27"
+ "12"
+ "05"
+ "2c"
+
+julia> randomNumbers(5, max=4096, base=2, min=2048)
+5-element Array{SubString{String},1}:
+ "0110011101101"
+ "0110101001011"
+ "0100101001110"
+ "0101000001110"
+ "0101101101100"
+
+julia> randomNumbers(5, max=200)
+5-element Array{Int64,1}:
+  69
+ 107
+  69
+ 155
+  72
+
+julia> randomNumbers(5, max=200, numeric=false)
+5-element Array{SubString{String},1}:
+ "121"
+ "178"
+ "155"
+ "12" 
+ "71"
+```
 """
-function randomNumbers(n = 100::Number; min = 1, max = 20, base = 10, check = true, col = 5) 
+function randomNumbers(n = 100::Number; min = 1, max = 20, base = 10, numeric = true, check = true, col = 5) 
     (n < 1 || n > 10000) && return "Requests must be between 1 and 10,000 numbers"
 
     (min < -1f+09 || max > 1f+09 || min > max) && return "Range must be between -1e9 and 1e9"
@@ -43,21 +85,36 @@ function randomNumbers(n = 100::Number; min = 1, max = 20, base = 10, check = tr
     urlbase = "https://www.random.org/integers/"
     urltxt = @sprintf("%s?num=%d&min=%d&max=%d&col=%d&base=%d&format=plain&rnd=new",
                     urlbase, n, Int(min), Int(max), col, base)
-#     print(urltxt)
-    response = HTTP.get( urltxt)
-    return [parse(Int64, x) for x in split(rstrip(String(response.body)))]
-
+    response = HTTP.get(urltxt)
+    (numeric && base == 10) && return [parse(Int, x) for x in split(rstrip(String(response.body)))]
+    return split(rstrip(String(response.body)))
 end;
 
 """
-    Get a randomized interval `[min, max]` -- (max - min + 1) randomized integers
-    All numbers are returned as strings (as Random.org sends them).
+Get a randomized interval `[min, max]`. Returns (max - min + 1) randomized integers
+All numbers are returned as strings (as Random.org sends them).
 
-    # Arguments
-    `check::Bool`: perform a call to `checkQuota` before making request
-    `col::Integer`: used to fulfill parameter requirments of random.org
+# Arguments
+- `min` : no less than 1
+- `max` : must be [-1e9, 1e9]
+- `col::Integer` : used to fulfill parameter requirments of random.org
+- `check::Bool`: perform a call to `checkQuota` before making request
+
+# Example
+```
+julia> randomSequence(max=8)
+8-element Array{Int64,1}:
+ 4
+ 1
+ 5
+ 3
+ 6
+ 7
+ 8
+ 2
+```
 """
-function randomSequence(;min = 1::Number, max = 20::Number, col = 5, check = true)
+function randomSequence(;min = 1::Number, max = 20::Number, col = 1, check = true)
     (min < -1f+09 || max > 1f+09 || min > max) && return "Range must be between -1e9 and 1e9"
 
     (check && !checkQuota()) && return "random.org suggests to wait until tomorrow"
@@ -70,13 +127,35 @@ function randomSequence(;min = 1::Number, max = 20::Number, col = 5, check = tru
 end;
 
 """
-    Get `n` random strings of length `len`
 
-    # Arguments
-    `digits::Bool`: include digits in strings
-    `upperalpha::Bool`: include upper case letters in strings
-    `loweralpha::Bool`: include lowercase letters in strings
-    `unique::Bool`: strings must be unique if `true`
+    randomStrings(n=10::Number, len=5; digits=true, upperalpha=true, loweralpha=true, unique=true, check=true)
+
+Get `n` random strings of length `len`
+
+# Arguments
+- `len` : length of strings [1, 20]
+- `digits::Bool`: include digits in strings
+- `upperalpha::Bool`: include upper case letters in strings
+- `loweralpha::Bool`: include lowercase letters in strings
+- `unique::Bool`: strings must be unique if `true`
+- `check::Bool`: perform a call to `checkQuota` before making request
+
+# Examples
+```
+julia> randomStrings(5, 8, loweralpha=false)
+5-element Array{SubString{String},1}:
+ "8LPPSHYL"
+ "Y3I7ILXJ"
+ "0O9C41EK"
+ "Y2N7U9WZ"
+ "OH9EZ3ST"
+
+julia> randomStrings(3, 4, digits=false)
+3-element Array{SubString{String},1}:
+ "cKIG"
+ "HUir"
+ "gAzX" 
+```
 """
 function randomStrings(n=10::Number, len=5; digits=true, upperalpha=true, loweralpha=true, unique=true, check=true)
     (n < 1 || n > 10000) && return "1 to 10,000 requests only"
@@ -94,17 +173,37 @@ function randomStrings(n=10::Number, len=5; digits=true, upperalpha=true, lowera
                 urlbase, n, len, ifelse(digits, "on", "off"),
                 ifelse(upperalpha, "on", "off"), ifelse(loweralpha, "on", "off"),
                 ifelse(unique, "on", "off"))
-#     print(urltxt)
     response = HTTP.get( urltxt)
     split(rstrip(String(response.body)))
 end;
 
 """
-    Get n numbers from a Gaussian distribution with `mean` and `stdev`.
-    Returns strings in `dec` decimal places.
-    Scientific notation only for now.
+
+    randomGaussian(n=10::Number, mean=0.0, stdev=1.0; dec=10, col=1, notation="scientific", check=true)
+
+Get n numbers from a Gaussian distribution with `mean` and `stdev`.
+Returns strings in `dec` decimal places.
+Scientific notation only for now.
+
+# Arguments
+- `mean`, `stdev` : between [-1e6, 1e6]
+- `dec1` : decimal places [2,20]
+- `col` : unique value
+- `scientific` : unique value
+- `check::Bool`: perform a call to `checkQuota` before making request
+
+# Example
+```
+julia> randomGaussian(5, dec=5)
+5-element Array{Float64,1}:
+ -0.36174
+  0.95992
+  1.2278 
+  1.0429 
+  0.31885
+```
 """
-function randomGaussian(n=10::Number, mean=0.0, stdev=1.0; dec=10, col=2, notation="scientific", check=true)
+function randomGaussian(n=10::Number, mean=0.0, stdev=1.0; dec=10, col=1, notation="scientific", check=true)
     (n < 1 || n > 10000) && return "Requests must be between 1 and 10,000 numbers"
 
     (mean < -1f+06 || mean > 1f+06) && return "Mean must be between -1e6 and 1e6"
@@ -124,8 +223,26 @@ function randomGaussian(n=10::Number, mean=0.0, stdev=1.0; dec=10, col=2, notati
 end;
 
 """
-    Get n decimal fractions on the interval (0,1).
-    Returns strings in `dec` decimal places.
+    randomDecimalFractions(n=10::Number; dec=10, col=2, check=true)
+
+Get n decimal fractions on the interval (0,1).
+Returns strings in `dec` decimal places.
+
+# Arguments
+- `dec` : decimal places [2, 20]
+- `col` : unique value
+- `check::Bool`: perform a call to `checkQuota` before making request
+
+# Example
+```
+julia> randomDecimalFractions(5, dec=3)
+5-element Array{Float64,1}:
+ 0.788
+ 0.949
+ 0.773
+ 0.875
+ 0.552
+```
 """
 function randomDecimalFractions(n=10::Number; dec=10, col=2, check=true)
     (n < 1 || n > 10000) && return "Requests must be between 1 and 10,000 numbers"
@@ -143,19 +260,44 @@ function randomDecimalFractions(n=10::Number; dec=10, col=2, check=true)
 end;
 
 """
-    Get n random bytes.
-    Returns strings in binary, decimal, octal, or hexadecimal.
-    The request will also download a DMS file.
+    randomBytes(n=10::Number; format="o", check=true)
+
+Get n random bytes.
+Returns strings in binary, decimal, octal, or hexadecimal.
+Returns hexadecimal numbers for "file."
+
+# Arguments
+- `format` : format of bytes; one of b, d, o, h, or file (Int in hex)
+- `check::Bool`: perform a call to `checkQuota` before making request
+
+# Examples
+```
+julia> randomBytes(5, format='o')
+5-element Array{SubString{String},1}:
+ "075"
+ "030"
+ "317"
+ "277"
+ "356
+
+julia> randomBytes(5, format="file")
+5-element Array{UInt8,1}:
+ 0xda
+ 0xdf
+ 0xd9
+ 0xb3
+ 0x97
+```
 """
-function randomBytes(n=10::Number; format="o", check=true)
+function randomBytes(n=10::Number; format='o', check=true)
     (n < 1 || n > 10000) && return "Requests must be between 1 and 10,000 numbers"
 
-    (!(format in ["b", "d", "o", "h", "file"])) && return "Format must be one of b, d, o, h, or file."
+    (!(format in ['b', 'd', 'o', 'h', "file"])) && return "Format must be one of b, d, o, h, or file."
 
     (check && !checkQuota()) && return "random.org suggests to wait until tomorrow"
 
     urlbase = "https://www.random.org/cgi-bin/randbyte"
-    urltxt = @sprintf("%s?nbytes=%d^=&format=%s",
+    urltxt = @sprintf("%s?nbytes=%d&format=%s",
                         urlbase, n, format)
     # print(urltxt)
     # response = HTTP.get( urltxt)
@@ -163,19 +305,22 @@ function randomBytes(n=10::Number; format="o", check=true)
     if (format == "file")
         return response.body
     else 
-        return [parse(Int64, x) for x in split(rstrip(String(response.body)))]
+        return split(rstrip(String(response.body)))
     end
 end
 
 """
-    Get a random bitmap.
-    Returns bitmap as PNG or GIF.
+    randomBitmap([format="png"; width=64, height=64, save="", overwrite='n', check=true])
 
-    # Arguments
-    `format::String`: png or gif
-    `height, width`: 1-300 pixels in each dimension
-    `save`: filename/filepath to write file (extension added by default)
-    `overwrite`: will not overwrite by default; set to 'y' to overwrite
+Get a random bitmap as a PNG or GIF.
+
+# Arguments
+- `format::String`: png or gif
+- `height, width`: 1-300 pixels in each dimension
+- `save`: filename/filepath to write file (extension added by default)
+- `overwrite`: will not overwrite by default; set to 'y' to overwrite
+- `check::Bool`: perform a call to `checkQuota` before making request
+
 """
 function randomBitmap(format="png"; width=64, height=64, save="", overwrite='n', check=true)
     (!(format in ["png", "gif"])) && return "Format must be png or gif."
